90739bc2f3eb469bc06004728825559965195ff0
Connor Yoshimoto <connory@evenset.com>
2020-01-13 14:34:46 -0500

    Add eslint configs

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: true,
+    semi: true,
+    jsxBracketSameLine: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 140,
+    tabWidth: 4,
+};
