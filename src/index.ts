341fd5e56f66ae6a7edce3fd6befd8ebb1caa176
Connor Yoshimoto <connory@evenset.com>
2020-02-11 16:04:07 -0500

    Add button

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import Example from './Example';
 import * as Localization from './contexts/LocalizationContext';
 import Text from './Text';
 import AvoidKeyboard from './AvoidKeyboard';
+import Button from './Button';
 
-export { Example, Localization, Text, AvoidKeyboard };
+export { Example, Localization, Text, Button, AvoidKeyboard };
