354801beb19f98e6458b281a03f296264689a1b4
Connor Yoshimoto <connory@evenset.com>
2020-02-11 16:05:57 -0500

    Add text input

---
+++ src/TextInput.tsx
@@ -0,0 +1,114 @@
+import React, { ReactElement } from 'react';
+import { StyleSheet, View, Text, TextInput, TextInputProps, StyleProp, ViewStyle, TextStyle } from 'react-native';
+import { LocalizationConsumer } from './contexts/LocalizationContext';
+
+const styles = StyleSheet.create({
+    text: {
+        fontWeight: 'normal',
+        paddingLeft: 15,
+        textAlignVertical: 'center',
+        paddingTop: 10,
+    },
+    label: {
+        fontWeight: 'normal',
+        width: '30%',
+        marginLeft: -15,
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '80%',
+    },
+    // eslint-disable-next-line react-native/no-color-literals
+    bubble: {
+        borderRadius: 10,
+        // FIXME: Move to color stylesheet once it is implemented
+        backgroundColor: 'grey',
+        width: '80%',
+        height: 40,
+    },
+    // eslint-disable-next-line react-native/no-color-literals
+    error: {
+        // FIXME: Move to color stylesheet once it is implemented
+        color: 'red',
+    },
+    column: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        marginBottom: 10,
+        height: 55,
+    },
+    errorRow: {
+        marginLeft: '20%',
+        alignContent: 'center',
+    },
+});
+
+export interface Props extends TextInputProps {
+    screen?: string;
+    errorMessageScreen?: string;
+    label?: string;
+    styleText?: StyleProp<TextStyle>;
+    styleLabel?: StyleProp<TextStyle>;
+    styleRow?: StyleProp<ViewStyle>;
+    styleBubble?: StyleProp<ViewStyle>;
+    errorMessage?: string;
+    styleError?: StyleProp<TextStyle>;
+    styleColumn?: StyleProp<ViewStyle>;
+    styleErrorRow?: StyleProp<ViewStyle>;
+    styleInnerErrorRow?: StyleProp<ViewStyle>;
+}
+
+class DefaultTextInput extends React.PureComponent<Props> {
+    render(): ReactElement {
+        const {
+            screen,
+            styleLabel,
+            styleRow,
+            styleText,
+            styleBubble,
+            label,
+            placeholder,
+            errorMessage,
+            errorMessageScreen,
+            styleError,
+            styleColumn,
+            styleErrorRow,
+            styleInnerErrorRow,
+            ...other
+        } = this.props;
+
+        // Adjusts the styling of the error message to match the placement of the input field automatically.
+        // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+        // @ts-ignore: ViewStyle should have a width element but TS is not picking it up correctly
+        const errorWidth = styleBubble && styleBubble.width ? styleBubble.width : styles.bubble.width;
+        const errorScreen = errorMessageScreen ? errorMessageScreen : screen;
+
+        return (
+            <LocalizationConsumer>
+                {({ translate }): ReactElement => (
+                    <View style={[styles.column, styleColumn]}>
+                        <View style={[styles.row, styleRow]}>
+                            {label ? <Text style={[styles.label, styleLabel]}>{translate(`${screen}.${label}`)}</Text> : null}
+                            <View style={[styles.bubble, styleBubble]}>
+                                <TextInput
+                                    placeholderTextColor="lightgrey"
+                                    {...other}
+                                    placeholder={translate(`${screen}.${placeholder}`)}
+                                    style={[styles.text, styleText]}
+                                ></TextInput>
+                            </View>
+                        </View>
+                        <View style={[styles.errorRow, styleErrorRow]}>
+                            <View style={[{ width: errorWidth }, styleInnerErrorRow]}>
+                                <Text style={[styles.error, styleError]}>{translate(`${errorScreen}.${errorMessage}`)}</Text>
+                            </View>
+                        </View>
+                    </View>
+                )}
+            </LocalizationConsumer>
+        );
+    }
+}
+export default DefaultTextInput;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import Example from './Example';
 import * as Localization from './contexts/LocalizationContext';
 import Text from './Text';
+import TextInput from './TextInput';
 import AvoidKeyboard from './AvoidKeyboard';
 import Button from './Button';
 
-export { Example, Localization, Text, Button, AvoidKeyboard };
+export { Example, Localization, Text, TextInput, Button, AvoidKeyboard };
