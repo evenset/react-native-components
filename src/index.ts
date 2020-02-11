4952325147fed173de84e4be0998d924a843cbcb
Connor Yoshimoto <connory@evenset.com>
2020-02-11 16:02:57 -0500

    Add keyboard avoider for iOS

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Example from './Example';
 import * as Localization from './contexts/LocalizationContext';
 import Text from './Text';
+import AvoidKeyboard from './AvoidKeyboard';
 
-export { Example, Localization, Text };
+export { Example, Localization, Text, AvoidKeyboard };
